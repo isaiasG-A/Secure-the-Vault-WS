221f5b429daa63804768211dc9add4d073d92545
Isaias Garcia <isaias@Isaiass-MacBook-Air.local>
2023-03-23 14:35:50 -0700

    third variable containing a number code has been created using the modulus operator

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-console.log("hello")
+//The puprose of this webpage is to generate a secret combination code for the user.
+
+//1. String message assigned to secretMessage "const" variable will be part of the dialog box.
+
+const secretMessage = "You have received this message because you have been chosen to open an important vault. Here is the secret combination:";
+
+//2. First code will be provided by creating a an arithmethic operation using the multiplication operator and assigning such  operation to a "const" varible called "productOp";
+
+const productOp = 2 * 3;
+
+//3. Second code will be provided by creating an arithmethic operation using th % modulus operator and assgining the operation to a const variable called modOp
+
+const modOp = 35 % 2;
+
